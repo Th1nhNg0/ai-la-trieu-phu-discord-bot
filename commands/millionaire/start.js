53a01764326a2b2fc6ad6c73da565b899032ae65
Th1nhNg0 <ngophuthinh2001@gmail.com>
2019-12-22 16:52:06 +0700

    add sound

--- commands/millionaire/start.js
+++ commands/millionaire/start.js
@@ -33,130 +33,151 @@ module.exports = {
   description: 'Start the game',
   run: async (client, message, args) => {
     let state = await db.get('state').value();
-    if (state === 'playing') {
-      await message.reply('ANOTHER GAME IS PLAYING!!!!');
-      return;
-    }
-    if (state !== 'init') {
-      await message.reply('The game not setup yet');
-      return;
-    }
+    if (state === 'playing')
+      return message.reply('ANOTHER GAME IS PLAYING!!!!');
+    if (state !== 'init') return message.reply('The game not setup yet');
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel)
+      return message.channel.send('You need to be in a voice channel to play!');
+    let connection = await voiceChannel.join();
+
     await db.set('state', 'playing').write();
     let questions = await db.get('questions').value();
     let players = await db.get('alivePlayers').value();
+    let dispatcher = connection.playFile('./music/start.ogg', {
+      type: 'ogg/opus'
+    });
+    dispatcher.on('end', async () => {
+      for (let i = 1; i <= 15; i++) {
+        if (state === 'end') {
+          await db.set('state', 'end').write();
+          let embed = await getEndEmbed();
+          return message.channel.send(embed);
+        }
+        dispatcher = connection.playFile('./music/1-5.ogg', {
+          type: 'ogg/opus'
+        });
+        const msg = await message.channel.send(
+          `:computer: LOADING QUESTION....`
+        );
+        try {
+          await msg.react('🇦');
+          await msg.react('🇧');
+          await msg.react('🇨');
+          await msg.react('🇩');
+        } catch (err) {
+          console.log('One of the emojis failed to react.');
+        }
 
-    for (let i = 1; i <= 15; i++) {
-      if (state === 'end') {
-        let embed = await getEndEmbed();
-        message.channel.send(embed);
-        return;
-      }
-      const msg = await message.channel.send(`:computer: LOADING QUESTION....`);
-      try {
-        await msg.react('🇦');
-        await msg.react('🇧');
-        await msg.react('🇨');
-        await msg.react('🇩');
-      } catch (err) {
-        console.log('One of the emojis failed to react.');
-      }
+        let question = questions[i - 1];
+        let answer = question.answer;
 
-      let question = questions[i - 1];
-      let answer = question.answer;
+        await msg.edit(
+          new Discord.RichEmbed()
+            .setColor('#53b512')
+            .setTitle('Câu hỏi số ' + i)
+            .setTimestamp()
+            .setFooter(
+              'Ai la trieu phu',
+              'https://upload.wikimedia.org/wikipedia/en/f/fe/Vietnam_millionaire.JPG'
+            )
+            .setDescription(question.question)
+            .addField('Đáp án 🇦', question.A, true)
+            .addField('Đáp án 🇧', question.B, true)
+            .addBlankField()
+            .addField('Đáp án 🇨', question.C, true)
+            .addField('Đáp án 🇩', question.D, true)
+        );
 
-      await msg.edit(
-        new Discord.RichEmbed()
-          .setColor('#53b512')
-          .setTitle('Câu hỏi số ' + i)
-          .setTimestamp()
-          .setFooter(
-            'Ai la trieu phu',
-            'https://upload.wikimedia.org/wikipedia/en/f/fe/Vietnam_millionaire.JPG'
-          )
-          .setDescription(question.question)
-          .addField('Đáp án 🇦', question.A, true)
-          .addField('Đáp án 🇧', question.B, true)
-          .addBlankField()
-          .addField('Đáp án 🇨', question.C, true)
-          .addField('Đáp án 🇩', question.D, true)
-      );
-
-      let users = {};
-      const filter = (reaction, user) => {
-        if (user.bot || user.id in users || !players.includes(user.username))
-          return false;
-        users[user.id] = {
-          answer: reaction.emoji.name,
-          username: user.username
+        let users = {};
+        const filter = (reaction, user) => {
+          if (user.bot || user.id in users || !players.includes(user.username))
+            return false;
+          users[user.id] = {
+            answer: reaction.emoji.name,
+            username: user.username
+          };
+          message.channel.send(user + ' ' + reaction.emoji.name);
+          return ['🇦', '🇧', '🇨', '🇩'].includes(reaction.emoji.name);
         };
-        message.channel.send(user + ' ' + reaction.emoji.name);
-        return ['🇦', '🇧', '🇨', '🇩'].includes(reaction.emoji.name);
-      };
-      await msg.awaitReactions(filter, { time: 15000 }).then(collected => {
-        if (collected.size === 0) {
-          state = 'end';
-          message.channel.send(`No one voted so bye :(`);
-        }
-        for (let id in users) {
-          if (users.hasOwnProperty(id)) {
-            let userAnswer = users[id].answer;
-            switch (userAnswer) {
-              case '🇦':
-                userAnswer = 'A';
-                break;
-              case '🇧':
-                userAnswer = 'B';
-                break;
-              case '🇨':
-                userAnswer = 'C';
-                break;
-              default:
-                userAnswer = 'D';
+        await msg
+          .awaitReactions(filter, { time: 30000 })
+          .then(async collected => {
+            if (collected.size === 0) {
+              state = 'end';
+              message.channel.send(`No one voted so bye :(`);
             }
-            if (userAnswer !== answer) {
-              let index = players.indexOf(users[id].username);
-              if (index > -1) {
-                players.splice(index, 1);
-                db.set('playerQues.' + users[id].username, i).write();
+            let correctAnswer = 0;
+            for (let id in users) {
+              if (users.hasOwnProperty(id)) {
+                let userAnswer = users[id].answer;
+                switch (userAnswer) {
+                  case '🇦':
+                    userAnswer = 'A';
+                    break;
+                  case '🇧':
+                    userAnswer = 'B';
+                    break;
+                  case '🇨':
+                    userAnswer = 'C';
+                    break;
+                  default:
+                    userAnswer = 'D';
+                }
+                if (userAnswer !== answer) {
+                  let index = players.indexOf(users[id].username);
+                  if (index > -1) {
+                    players.splice(index, 1);
+                    db.set('playerQues.' + users[id].username, i).write();
+                  } else {
+                    correctAnswer++;
+                  }
+                }
               }
+              db.set('quesNum', i).write();
+              db.set('alivePlayers', players).write();
+              if (players.length === 0) state = 'end';
+              let fileName;
+              if (correctAnswer >= users.length / 2) fileName = '1-5dung.ogg';
+              else fileName = '1-5sai.ogg';
+              dispatcher.end();
+              dispatcher = connection.playFile('./music/' + fileName, {
+                type: 'ogg/opus'
+              });
+              message.channel.send(
+                'OUR ANWSER ISSSS:\n :regional_indicator_' +
+                  answer.toLocaleLowerCase() +
+                  ':'
+              );
+              message.channel.send(
+                new Discord.RichEmbed()
+                  .setColor('#0099ff')
+                  .setTitle('Người chơi tiếp: ' + players.length)
+                  .setTimestamp()
+                  .setFooter(
+                    'Ai la trieu phu',
+                    'https://upload.wikimedia.org/wikipedia/en/f/fe/Vietnam_millionaire.JPG'
+                  )
+                  .addField(
+                    'Danh sách',
+                    players.length > 0 ? players.join('/n') : "Đéo có ai :'("
+                  )
+              );
+              await sleep(6000);
             }
-          }
-          db.set('quesNum', i).write();
-          db.set('alivePlayers', players).write();
-          if (players.length === 0) state = 'end';
+          });
 
-          message.channel.send(
-            'OUR ANWSER ISSSS:\n :regional_indicator_' +
-              answer.toLocaleLowerCase() +
-              ':'
-          );
-          message.channel.send(
-            new Discord.RichEmbed()
-              .setColor('#0099ff')
-              .setTitle('Người chơi tiếp: ' + players.length)
-              .setTimestamp()
-              .setFooter(
-                'Ai la trieu phu',
-                'https://upload.wikimedia.org/wikipedia/en/f/fe/Vietnam_millionaire.JPG'
-              )
-              .addField(
-                'Danh sách',
-                players.length > 0 ? players.join('/n') : "Đéo có ai :'("
-              )
-          );
-        }
-      });
-
-      // if not lag
-      // let count = 15;
-      // const counter = setInterval(() => {
-      //   if (count > 0) {
-      //     msg.edit(count);
-      //     count--;
-      //   } else {
-      //     clearInterval(counter);
-      //   }
-      // }, 1000);
-    }
+        // if not lag
+        // let count = 15;
+        // const counter = setInterval(() => {
+        //   if (count > 0) {
+        //     msg.edit(count);
+        //     count--;
+        //   } else {
+        //     clearInterval(counter);
+        //   }
+        // }, 1000);
+      }
+    });
   }
 };
